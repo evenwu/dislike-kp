818d091102fb62e1cd9b7f6380f2c2fc2514fe3f
WeiChiaChang <ioh04019@gmail.com>
2019-09-08 00:25:16 +0800

    ✨ Feat: First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="content">
+      <h1>退讚，同場加映韓導</h1>
+      <p>邀請朋友共襄盛舉吧 👎</p>
+    </div>
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FDoctorKoWJ&tabs&width=340&height=214&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="214" style="border:none;overflow:hidden"
+      scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftwherohan%2F&tabs&width=340&height=214&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="340" height="214" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    <div class="footer">
+      <p>本頁由 <a target="_blank" href="https://github.com/WeiChiaChang">WeiChiaChang</a> 製作。</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  name: 'app'
 }
 </script>
 
 <style>
+html, body {
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  background-image: url(https://upload.cc/i1/2019/09/07/9pZ7L8.gif);
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  min-width: 50%;
+  min-height: 50%;
+  opacity: 1;
+  width: 100%;
+  max-width: 100%;
+  height: auto;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.content {
+  color: white;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+.footer {
+  color: white;
+  font-size: .75rem;
+}
+
+.footer a {
+  text-decoration: none;
+  color: white;
 }
 </style>
